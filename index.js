9bf8458d2aa40947e50e26e45bc1e5472eb479e5
Juliana Kang <julianajlk@gmail.com>
2018-05-11 13:49:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
 }
 
 function deleteFromObjectByKey(obj, key) {
-  var newObj = Object.assign({[key]:value}, recipes); 
+  var newObj = Object.assign({}, obj); 
   delete newObj[key];
   return newObj;
 }
