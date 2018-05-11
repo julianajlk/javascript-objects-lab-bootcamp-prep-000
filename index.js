917bc917305ded758d78081717c6903bc2728313
Juliana Kang <julianajlk@gmail.com>
2018-05-11 13:38:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   var newObj = Object.assign({}, recipes);
   return newObj;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return object;
+}
